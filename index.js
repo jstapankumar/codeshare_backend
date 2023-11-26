97d050420f0a11b3a893fd053ea6b1fdf4ba131d
stapankumar <stapankumar743@gmail.com>
2023-11-26 20:03:06 +0530

    Cors handled

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ const errorHandler = require("./middlewares/error")
 const app = express();
 
 //middleware
-app.use(cors())
+let whitelist = ['https://code-shareapp.vercel.app']; //white list consumers
+let corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/posts", postRoutes);
